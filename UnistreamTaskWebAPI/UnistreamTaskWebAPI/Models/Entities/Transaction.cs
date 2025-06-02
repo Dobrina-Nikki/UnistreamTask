537d0bbc6c451db24cdd856a01c6757d72993f66
Gromov Nikita <gromstormix@gmail.com>
2025-06-02 21:52:39 +0300

    fix

---
+++ UnistreamTaskWebAPI/UnistreamTaskWebAPI/Models/Entities/Transaction.cs
@@ -0,0 +1,10 @@
+﻿namespace UnistreamTaskWebAPI.Models.Entities
+{
+    public class Transaction
+    {
+        public Guid Id { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime InsertDateTime { get; set; }
+    }
+}
---
+++ UnistreamTaskWebAPI/UnistreamTaskWebAPI/Services/Interfaces/ITransactionService.cs
@@ -0,0 +1,10 @@
+﻿using UnistreamTaskWebAPI.Models;
+
+namespace UnistreamTaskWebAPI.Services.Interfaces
+{
+    public interface ITransactionService
+    {
+        Task<TransactionResponse> CreateTransactionAsync(Transaction inputTransaction);
+        Task<Transaction?> GetTransactionAsync(Guid id);
+    }
+}
